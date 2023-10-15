45009331d94b923039418ca5ec4152bf71f2d91b
Robert Koval <kovalrobert@me.com>
2023-10-15 16:12:17 +0300

    Initial Commit

---
+++ ADXL345_Client/ADXL345_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADXL345_ClientApp.swift
+//  ADXL345_Client
+//
+//  Created by Robert Koval on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ADXL345_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADXL345_Client/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADXL345_Client
+//
+//  Created by Robert Koval on 15.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
