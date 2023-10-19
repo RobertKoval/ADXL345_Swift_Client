117c8251e36547a82b8939b1e89e81b34035716b
Robert Koval <kovalrobert@me.com>
2023-10-19 20:21:49 +0300

    Simplified everything

--- ADXL345_Client/ContentView.swift
+++ ADXL345_Client/ContentView.swift
@@ -10,44 +10,69 @@ import Charts
 
 struct ContentView: View {
     @ObservedObject var wsService = WebsocketService()
-
+    
     var body: some View {
         VStack {
-            Chart(wsService.dataPoints.suffix(1000)) { point in
-                LineMark(x: .value("Time", point.date),
-                         y: .value("Value", point.value)
-                )
-                .foregroundStyle(by: .value("Axis", point.axis))
-                .interpolationMethod(.cardinal)
+            Picker("Record Options", selection: $wsService.recordOptions) {
+                ForEach([RecordOptions.combined, .splitted, .all], id: \.self) { option in
+                    if option.contains(.all) {
+                        Text("All").tag(option)
+                    } else if option.contains(.combined) {
+                        Text("Combined").tag(option)
+                    } else {
+                        Text("Splitted").tag(option)
+                    }
+                }
             }
-
-            Chart(wsService.combinedDataPoints.suffix(1000)) { point in
-                LineMark(x: .value("Time", point.date),
-                         y: .value("Value", point.value)
-                )
-                .interpolationMethod(.cardinal)
+            .pickerStyle(SegmentedPickerStyle())
+            .disabled(wsService.isConnected)
+            
+            Picker("Sample Size", selection: $wsService.recordingSize) {
+                Text("128").tag(128)
+                Text("256").tag(256)
+                Text("512").tag(512)
+                Text("1024").tag(1024)
+                
             }
-
-            if (!wsService.fourier.isEmpty) {
-                Chart(wsService.fourier.suffix(1000)) { point in
-                    LineMark(x: .value("Frequency", point.frequency),
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            .disabled(wsService.isConnected)
+            
+            
+            if wsService.recordOptions.contains(.combined) {
+                Chart(wsService.combinedDataPoints) { point in
+                    LineMark(x: .value("Time", point.date),
                              y: .value("Value", point.value)
                     )
                     .interpolationMethod(.cardinal)
                 }
+                .padding()
             }
-
+            
+            if wsService.recordOptions.contains(.splitted) {
+                Chart(wsService.dataPoints) { point in
+                    LineMark(x: .value("Time", point.date),
+                             y: .value("Value", point.value)
+                    )
+                    .foregroundStyle(by: .value("Axis", point.axis))
+                    .interpolationMethod(.cardinal)
+                }
+                .padding()
+            }
+            
             Button("Connect") {
                 Task {
                     try await wsService.connect()
                 }
             }
+            .disabled(wsService.isConnected)
 
             Button("Disconnect") {
                 Task {
                     try await wsService.onDisconnect()
                 }
             }
+            .disabled(!wsService.isConnected)
         }
         .padding()
     }
--- ADXL345_Client/WebsocketService.swift
+++ ADXL345_Client/WebsocketService.swift
@@ -8,100 +8,71 @@
 import Foundation
 import WebSocketKit
 import NIOPosix
-import Accelerate
+import Collections
 
+struct RecordOptions: OptionSet, Equatable, Hashable, Identifiable {
+    let rawValue: Int
+    
+    var id: Int {
+        rawValue
+    }
+    
+    static let combined = RecordOptions(rawValue: 1 << 0)
+    static let splitted  = RecordOptions(rawValue: 1 << 1)
+    
+    static let all: RecordOptions = [.combined, .splitted]
+}
 
+@MainActor
 class WebsocketService: ObservableObject {
     let server = "ws://192.168.88.26:81"
-    let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-
-    @Published var dataPoints: [AccelerationDataPoint] = []
-    @Published var combinedDataPoints: [CombinedAcceleration] = []
-    @Published var fourier: [FourierTransformPoints] = []
-
+    let eventLoopGroup = MultiThreadedEventLoopGroup.singleton
+    
+    @Published var dataPoints: Deque<AccelerationDataPoint> = []
+    @Published var combinedDataPoints: Deque<CombinedAcceleration> = []
+    @Published var recordingSize: Int = 128
+    @Published var recordOptions: RecordOptions = .combined
+    @Published var isConnected = false
     var ws: WebSocket? = nil
-
+    
     func connect() async throws  {
-        DispatchQueue.main.async {
-            self.dataPoints = []
-            self.combinedDataPoints = []
-        }
+        self.dataPoints = []
+        self.combinedDataPoints = []
+
+
         try await WebSocket.connect(to: server, on: eventLoopGroup) {  [weak self] ws in
+            DispatchQueue.main.async {
+                self?.isConnected = true
+            }
+            
             ws.onText { ws, json in
                 let acceleration = try! JSONDecoder().decode(Acceleration.self, from: json.data(using: .utf8)!)
-
+                
                 DispatchQueue.main.async {
                     guard let self else {return}
-                    self.dataPoints.append(contentsOf: unpack(acceleration))
-                    self.combinedDataPoints.append(combine(acceleration))
-
-                    if (isPowerOfTwo(self.combinedDataPoints.count)) {
-                        self.fourier = performFourierTransform(combinedDataPoints: self.combinedDataPoints)
+                    
+                    if self.recordOptions.contains(.combined) {
+                        self.combinedDataPoints.append(combine(acceleration))
+                        if self.combinedDataPoints.count > self.recordingSize {
+                            self.combinedDataPoints.removeFirst()
+                        }
+                    }
+                    
+                    if self.recordOptions.contains(.splitted) {
+                        self.dataPoints.append(contentsOf: unpack(acceleration))
+                        if self.dataPoints.count > self.recordingSize {
+                            self.dataPoints.removeFirst(3)
+                        }
                     }
                 }
-
-                print(json)
             }
-
+            
             self?.ws = ws
         }.get()
     }
 
     func onDisconnect() async throws {
         try await ws?.close()
-        
+        isConnected = false
     }
-}
-
-func isPowerOfTwo(_ n: Int) -> Bool {
-    return n > 0 && (n & (n - 1)) == 0
-}
-
-func calculateSampleRate(_ data: [CombinedAcceleration]) -> Double  {
-    guard  data.count > 1 else { return 0 }
-
-    let firstDate = data.first!.date
-    let lastDate = data.last!.date
-    let totalTime = lastDate.timeIntervalSince(firstDate)
-    let averageInterval = totalTime / Double(data.count - 1)
-    return 1.0 / averageInterval
-}
-
-struct FourierTransformPoints: Identifiable {
-    let id = UUID()
-    let value: Double
-    let frequency: Double
-}
-
-func performFourierTransform(combinedDataPoints: [CombinedAcceleration]) -> [FourierTransformPoints] {
-    let values = combinedDataPoints.map(\.value)
-    let N = values.count
-    let samplingRate = calculateSampleRate(combinedDataPoints)
-    let frequencyResolution = samplingRate / Double(N)
-
-    var real = [Double](repeating: 0.0, count: N)
-    var imaginary = [Double](repeating: 0.0, count: N)
-    var tempSplitComplex = DSPDoubleSplitComplex(realp: &real, imagp: &imaginary)
-
-    guard let setup = vDSP_DFT_zop_CreateSetupD(
-        nil,
-        vDSP_Length(N),
-        vDSP_DFT_Direction.FORWARD) else {
-        fatalError("can't create vDSP_DFT_Setup")
-    }
-    vDSP_DFT_ExecuteD(setup, values, [Double](repeating: 0.0, count: N), &real, &imaginary)
-    vDSP_DFT_DestroySetupD(setup)
-
-    var magnitudes = [Double](repeating: 0, count: N)
-    vDSP_zvmagsD(&tempSplitComplex, 1, &magnitudes, 1, vDSP_Length(N))
-
-    // Pairing the magnitudes with their corresponding frequencies
-    var result: [FourierTransformPoints] = []
-    for i in 0..<(N/2) { // We take only the first half for real signals
-        let frequency = frequencyResolution * Double(i)
-        let point = FourierTransformPoints(value: magnitudes[i], frequency: frequency)
-        result.append(point)
-    }
-
-    return result
 }
