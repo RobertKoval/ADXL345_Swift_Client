ffd0252e7b7109772434e028ee9d371b9bcba29d
Robert Koval <kovalrobert@me.com>
2023-10-24 13:55:12 +0300

    Minor improvements

--- ADXL345_Client/ContentView.swift
+++ ADXL345_Client/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 import Charts
 
 struct ContentView: View {
+    @AppStorage(UserDefaultsKeys.wsAddress.rawValue) var wsAddress = UserDefaultsKeys.wsAddress.defaultValue
     @ObservedObject var wsService = WebsocketService()
-    
+
     var body: some View {
         VStack {
+            ConnectView(url: $wsAddress) {
+                Task {
+                    try await wsService.onConnect()
+                }
+            }
+            .disabled(wsService.isConnected)
+
             Picker("Record Options", selection: $wsService.recordOptions) {
                 ForEach([RecordOptions.combined, .splitted, .all], id: \.self) { option in
                     if option.contains(.all) {
@@ -26,19 +34,19 @@ struct ContentView: View {
             }
             .pickerStyle(SegmentedPickerStyle())
             .disabled(wsService.isConnected)
-            
+
             Picker("Sample Size", selection: $wsService.recordingSize) {
                 Text("128").tag(128)
                 Text("256").tag(256)
                 Text("512").tag(512)
                 Text("1024").tag(1024)
-                
+
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
             .disabled(wsService.isConnected)
-            
-            
+
+
             if wsService.recordOptions.contains(.combined) {
                 Chart(wsService.combinedDataPoints) { point in
                     LineMark(x: .value("Time", point.date),
@@ -48,7 +56,7 @@ struct ContentView: View {
                 }
                 .padding()
             }
-            
+
             if wsService.recordOptions.contains(.splitted) {
                 Chart(wsService.dataPoints) { point in
                     LineMark(x: .value("Time", point.date),
@@ -59,13 +67,6 @@ struct ContentView: View {
                 }
                 .padding()
             }
-            
-            Button("Connect") {
-                Task {
-                    try await wsService.connect()
-                }
-            }
-            .disabled(wsService.isConnected)
 
             Button("Disconnect") {
                 Task {
@@ -75,6 +76,42 @@ struct ContentView: View {
             .disabled(!wsService.isConnected)
         }
         .padding()
+    }
+}
+
+struct ConnectView: View {
+    enum IPAddressField: Hashable {
+        case focused
+    }
+
+    @FocusState var focusField: IPAddressField?
+    @Binding var url: String
+    let onConnect: () -> Void
+
+    var body: some View {
+        HStack {
+            Section {
+                TextField("URL:Port", text: $url)
+                    .focused($focusField, equals: .focused)
+#if os(iOS)
+                    .keyboardType(.numbersAndPunctuation)
+#endif
+            }
+
+            Button("Connect") {
+                onConnect()
+            }
+            .disabled(url.isEmpty)
+        }
+
+        .onAppear(perform: {
+
+#if os(macOS)  // TODO: How to disable focused by default?
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                focusField = nil
+            }
+#endif
+        })
     }
 }
 
--- ADXL345_Client/WebsocketService.swift
+++ ADXL345_Client/WebsocketService.swift
@@ -10,65 +10,77 @@ import WebSocketKit
 import NIOPosix
 import Collections
 
+enum UserDefaultsKeys: String {
+    case wsAddress
+
+    var defaultValue: String {
+        switch self {
+        case .wsAddress:
+            return "192.168.88.26:81"
+        }
+    }
+}
+
 struct RecordOptions: OptionSet, Equatable, Hashable, Identifiable {
     let rawValue: Int
-    
+
     var id: Int {
         rawValue
     }
-    
+
     static let combined = RecordOptions(rawValue: 1 << 0)
     static let splitted  = RecordOptions(rawValue: 1 << 1)
-    
+
     static let all: RecordOptions = [.combined, .splitted]
 }
 
 @MainActor
 class WebsocketService: ObservableObject {
-    let server = "ws://192.168.88.26:81"
-    let eventLoopGroup = MultiThreadedEventLoopGroup.singleton
-    
+    var server: String {
+        "ws://\(UserDefaults.standard.string(forKey: UserDefaultsKeys.wsAddress.rawValue) ?? UserDefaultsKeys.wsAddress.defaultValue)"
+    }
+
     @Published var dataPoints: Deque<AccelerationDataPoint> = []
     @Published var combinedDataPoints: Deque<CombinedAcceleration> = []
     @Published var recordingSize: Int = 128
     @Published var recordOptions: RecordOptions = .combined
     @Published var isConnected = false
     var ws: WebSocket? = nil
-    
-    func connect() async throws  {
+
+    @Sendable
+    func onEvent(_ ws: WebSocket, _ json: String) async -> Void {
+        let acceleration = try! JSONDecoder().decode(Acceleration.self, from: json.data(using: .utf8)!)
+
+        if self.recordOptions.contains(.combined) {
+            self.combinedDataPoints.append(combine(acceleration))
+            if self.combinedDataPoints.count > self.recordingSize {
+                self.combinedDataPoints.removeFirst()
+            }
+        }
+
+        if self.recordOptions.contains(.splitted) {
+            self.dataPoints.append(contentsOf: unpack(acceleration))
+            if self.dataPoints.count > self.recordingSize {
+                self.dataPoints.removeFirst(3)
+            }
+        }
+    }
+
+    func onConnect() async throws  {
         self.dataPoints = []
         self.combinedDataPoints = []
 
-
-        try await WebSocket.connect(to: server, on: eventLoopGroup) {  [weak self] ws in
-            DispatchQueue.main.async {
-                self?.isConnected = true
+        ws = try await withCheckedThrowingContinuation { continuation in
+            do {
+                try WebSocket.connect(to: server, on: MultiThreadedEventLoopGroup.singleton) { ws in
+                    continuation.resume(returning: ws)
+                }.wait()
+            } catch {
+                continuation.resume(throwing: error)
             }
-            
-            ws.onText { ws, json in
-                let acceleration = try! JSONDecoder().decode(Acceleration.self, from: json.data(using: .utf8)!)
-                
-                DispatchQueue.main.async {
-                    guard let self else {return}
-                    
-                    if self.recordOptions.contains(.combined) {
-                        self.combinedDataPoints.append(combine(acceleration))
-                        if self.combinedDataPoints.count > self.recordingSize {
-                            self.combinedDataPoints.removeFirst()
-                        }
-                    }
-                    
-                    if self.recordOptions.contains(.splitted) {
-                        self.dataPoints.append(contentsOf: unpack(acceleration))
-                        if self.dataPoints.count > self.recordingSize {
-                            self.dataPoints.removeFirst(3)
-                        }
-                    }
-                }
-            }
-            
-            self?.ws = ws
-        }.get()
+        }
+        ws?.onText(onEvent)
+        isConnected = true
     }
 
     func onDisconnect() async throws {
